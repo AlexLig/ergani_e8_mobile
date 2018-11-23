f9d4fb71dc9ac8f144e215f5bd75daeef36a51d0
Kostas Liberopoulos <libekonst@gmail.com>
2018-11-23 23:08:18 +0200

    Implement add new employee.

--- lib/contacts/contacts_route.dart
+++ lib/contacts/contacts_route.dart
@@ -1,25 +1,16 @@
 import 'dart:async';
-import 'package:ergani_e8/contacts/delete_dialog.dart';
+import 'package:ergani_e8/components/empty_contacts_indicator.dart';
+import 'package:ergani_e8/components/delete_dialog.dart';
 import 'package:ergani_e8/contacts/drawer.dart';
-import 'package:ergani_e8/contacts/edit_dialog.dart';
+import 'package:ergani_e8/components/edit_dialog.dart';
 import 'package:ergani_e8/contacts/employee.dart';
+import 'package:ergani_e8/components/employee_list_tile.dart';
+import 'package:ergani_e8/create_employee_route.dart';
 import 'package:flutter/material.dart';
 
 class ContactsRoute extends StatefulWidget {
   @override
   ContactsRouteState createState() => ContactsRouteState();
-}
-
-class Employee {
-  String firstName;
-  String lastName;
-  String vatNumber;
-
-  Employee({
-    @required this.firstName,
-    @required this.lastName,
-    @required this.vatNumber,
-  });
 }
 
 class ContactsRouteState extends State<ContactsRoute> {
@@ -32,22 +23,6 @@ class ContactsRouteState extends State<ContactsRoute> {
         firstName: 'Ηλιάννα', lastName: 'Παπαγεωργίου', vatNumber: '222222222'),
     Employee(
         firstName: 'Ηλιάννα', lastName: 'Παπαγεωργίου', vatNumber: '333333333'),
-    Employee(
-        firstName: 'Ηλιάννα', lastName: 'Παπαγεωργίου', vatNumber: '444444444'),
-    Employee(
-        firstName: 'Ηλιάννα', lastName: 'Παπαγεωργίου', vatNumber: '555555555'),
-    Employee(
-        firstName: 'Ηλιάννα', lastName: 'Παπαγεωργίου', vatNumber: '666666666'),
-    Employee(
-        firstName: 'Ηλιάννα', lastName: 'Παπαγεωργίου', vatNumber: '777777777'),
-    Employee(
-        firstName: 'Ηλιάννα', lastName: 'Παπαγεωργίου', vatNumber: '888888888'),
-    Employee(
-        firstName: 'Ηλιάννα', lastName: 'Παπαγεωργίου', vatNumber: '999999999'),
-    Employee(
-        firstName: 'Ηλιάννα', lastName: 'Παπαγεωργίου', vatNumber: '000000000'),
-    Employee(
-        firstName: 'Ηλιάννα', lastName: 'Παπαγεωργίου', vatNumber: '123456789'),
   ];
 
   void _handleDelete({
@@ -64,24 +39,18 @@ class ContactsRouteState extends State<ContactsRoute> {
             firstName: firstName,
             lastName: lastName,
             onDelete: () {
-              try {
-                setState(() {
-                  employeeList = employeeList
-                      .where((el) => el.vatNumber != vatNumber)
-                      .toList();
-                });
-                Scaffold.of(scaffoldContext).showSnackBar(SnackBar(
-                  content: Text('Ο υπάλληλος διαγράφηκε.'),
+              setState(() {
+                employeeList = employeeList
+                    .where((el) => el.vatNumber != vatNumber)
+                    .toList();
+              });
+              Scaffold.of(scaffoldContext).showSnackBar(
+                SnackBar(
+                  content: Text('Ο/Η $lastName $firstName διαγράφηκε.'),
                   backgroundColor: Colors.green,
                   // TODO: Undo delete.
-                ));
-              } catch (e) {
-                Scaffold.of(scaffoldContext).showSnackBar(SnackBar(
-                  content: Text('Σφάλμα διαγραφής υπαλλήλου.'),
-                  backgroundColor: Colors.orange,
-                ));
-                print(e);
-              }
+                ),
+              );
               Navigator.of(context).pop();
             },
           );
@@ -119,15 +88,11 @@ class ContactsRouteState extends State<ContactsRoute> {
         ),
         title: Text('Υπάλληλοι'),
         actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: IconButton(
+          IconButton(
               tooltip: 'Προσθήκη Υπαλλήλου',
               icon: Icon(Icons.person_add, color: Colors.white),
               // SnackBar not working here. Need GlobalKey??
-              onPressed: () => _handleEdit(context),
-            ),
-          ),
+              onPressed: () => _handleSubmitEmployee(context)),
         ],
       ),
 
@@ -136,14 +101,13 @@ class ContactsRouteState extends State<ContactsRoute> {
       // Not the context of a child of Scaffold.
       // You can solve this by simply using a different context :
       body: Builder(builder: (context) {
-
         return Container(
-          color: employeeList.length == 0 ? Colors.grey[200] : Colors.white ,
+          color: employeeList.length == 0 ? Colors.grey[200] : Colors.white,
           child: Column(
             children: <Widget>[
               Expanded(
-                child: isLoading
-                    ? Center(child: CircularProgressIndicator())
+                child: employeeList.length == 0
+                    ? AddContactsIndicator()
                     : ListView.builder(
                         itemCount: employeeList.length,
                         itemBuilder: (BuildContext context, int i) {
@@ -162,8 +126,7 @@ class ContactsRouteState extends State<ContactsRoute> {
                                   );
                                 },
                                 onEdit: () => _handleEdit(context),
-                                onTap: () => _showSnackBar(
-                                    context, 'Την Ηλιάννα ρε λιγούρη;'),
+                                onTap: () => print(''),
                                 // overtimeStart: overtimeStart,
                               ),
                               i == employeeList.length - 1
@@ -180,5 +143,15 @@ class ContactsRouteState extends State<ContactsRoute> {
       }),
       drawer: ContactsDrawer(),
     );
+  }
+
+  _handleSubmitEmployee(context) async {
+    final newEmployee = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => EmployeeForm(),
+      ),
+    );
+    if (newEmployee is Employee) setState(() => employeeList.add(newEmployee));
   }
 }

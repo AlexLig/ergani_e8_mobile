1c386b8a30790d1492a5ab67bcdb6fab0c31ecd1
Kostas Liberopoulos <libekonst@gmail.com>
2018-11-28 00:21:57 +0200

    Add timeToStart text field and on blur validation.

--- lib/routes/create_employee_route.dart
+++ lib/routes/create_employee_route.dart
@@ -15,16 +15,21 @@ class EmployeeForm extends StatefulWidget {
 class EmployeeFormState extends State<EmployeeForm> {
   final _formKey = GlobalKey<FormState>();
   // FocusNode employeeFocusNode;
-  FocusNode nameFocus;
-  FocusNode surnameFocus;
+  FocusNode firstNameFocus;
+  FocusNode lastNameFocus;
   FocusNode vatNumberFocus;
+  FocusNode timeToStartFocus;
 
-  var _nameController = TextEditingController();
-  var _surnameController = TextEditingController();
+  var _firstNameController = TextEditingController();
+  var _lastNameController = TextEditingController();
   var _vatNumberController = TextEditingController();
+  var _timeToStartController = TextEditingController();
 
   Employee _employee;
-
+  bool _shouldValidateOnChangeFirstName = false;
+  bool _shouldValidateOnChangeLastName = false;
+  bool _shouldValidateOnChangeVatNumber = false;
+  bool _shouldValidateOnChangeTimeToStart = false;
   var _employeeList;
 
   @override
@@ -33,38 +38,66 @@ class EmployeeFormState extends State<EmployeeForm> {
     _employee = widget.employee;
 
     if (_employee != null) {
-      _nameController.text = _employee.firstName;
-      _surnameController.text = _employee.lastName;
+      _firstNameController.text = _employee.firstName;
+      _lastNameController.text = _employee.lastName;
       _vatNumberController.text = _employee.vatNumber;
+      // _timeToStartController.text =
+      //     '${_employee.hourToStart.hour}${_employee.hourToStart.minute}';
     }
-    nameFocus = FocusNode();
-    surnameFocus = FocusNode();
+    firstNameFocus = FocusNode();
+    lastNameFocus = FocusNode();
     vatNumberFocus = FocusNode();
+    timeToStartFocus = FocusNode();
+
+    // firstNameFocus.addListener(() {
+    //   if (!firstNameFocus.hasFocus) {
+    //     setState(() => _shouldValidateOnChangeFirstName = true);
+    //   }
+    // });
+    // lastNameFocus.addListener(() {
+    //   if (!lastNameFocus.hasFocus)
+    //     setState(() => _shouldValidateOnChangeLastName = true);
+    // });
+    // vatNumberFocus.addListener(() {
+    //   if (!vatNumberFocus.hasFocus)
+    //     setState(() => _shouldValidateOnChangeVatNumber = true);
+    // });
   }
 
   @override
   void dispose() {
-    nameFocus.dispose();
-    surnameFocus.dispose();
+    firstNameFocus.dispose();
+    lastNameFocus.dispose();
     vatNumberFocus.dispose();
-    _nameController.dispose();
-    _surnameController.dispose();
+    timeToStartFocus.dispose();
+    _firstNameController.dispose();
+    _lastNameController.dispose();
     _vatNumberController.dispose();
+    _timeToStartController.dispose();
     super.dispose();
   }
 
+// '${s[0].toUpperCase()}${s.substring(1)}'
   submit(context) {
     if (this._formKey.currentState.validate()) {
       //_formKey.currentState.save();
       Employee employeeToSubmit = Employee(
-          firstName: _nameController.text,
-          lastName: _surnameController.text,
-          vatNumber: _vatNumberController.text);
+        '${_firstNameController.text[0].toUpperCase()}${_firstNameController.text.substring(1)}',
+        '${_lastNameController.text[0].toUpperCase()}${_lastNameController.text.substring(1)}',
+        _vatNumberController.text,
+        TimeOfDay(
+          hour: int.tryParse(_timeToStartController.text.substring(0, 2)) ?? 00,
+          minute: int.tryParse(_timeToStartController.text.substring(2, 4)) ?? 00,
+        ),
+      );
+// int.tryParse returns null on invalid input. Can use ?? to check if null.
+// e.g. int val = int.tryParse(text) ?? defaultValue;
 
-      _nameController.clear();
-      _surnameController.clear();
+// TODO: directly write to the db, show loading in the midtime, then send ok via pop().
+      _firstNameController.clear();
+      _lastNameController.clear();
       _vatNumberController.clear();
-
+      _timeToStartController.clear();
       Navigator.pop(context, employeeToSubmit);
     }
   }
@@ -80,47 +113,84 @@ class EmployeeFormState extends State<EmployeeForm> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              // NAME Textfield
+              // FIRSTNAME Textfield
               TextFormField(
                 keyboardType: TextInputType.text,
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (value) {
-                  FocusScope.of(context).requestFocus(surnameFocus);
+                  FocusScope.of(context).requestFocus(lastNameFocus);
                 },
                 autofocus: true,
-                focusNode: nameFocus,
+                autovalidate: _shouldValidateOnChangeFirstName,
+                focusNode: firstNameFocus,
                 decoration: InputDecoration(labelText: 'Όνομα'),
                 validator: (value) {
                   if (value.isEmpty) {
                     return 'Προσθέστε όνομα';
                   }
                 },
-                controller: _nameController,
+                controller: _firstNameController,
               ),
-              // SURNAME textfield
+              // LASTNAME textfield
               TextFormField(
                 keyboardType: TextInputType.text,
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (value) {
-                  FocusScope.of(context).requestFocus(vatNumberFocus);
+                  if (value.isEmpty)
+                    setState(() => _shouldValidateOnChangeLastName = true);
+                  else
+                    FocusScope.of(context).requestFocus(vatNumberFocus);
                 },
-                focusNode: surnameFocus,
+                focusNode: lastNameFocus,
                 decoration: InputDecoration(labelText: 'Επίθετο'),
                 validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Προσθέστε επίθετο';
-                  }
+                  if (value.isEmpty) return 'Προσθέστε επίθετο';
                 },
-                controller: _surnameController,
+                autovalidate: _shouldValidateOnChangeLastName,
+                // onEditingComplete: () =>
+                controller: _lastNameController,
               ),
               // VATNUMBER textfield
               TextFormField(
                 keyboardType: TextInputType.number,
                 focusNode: vatNumberFocus,
-                textInputAction: TextInputAction.done,
+                textInputAction: TextInputAction.next,
                 decoration: InputDecoration(labelText: 'ΑΦΜ'),
                 validator: (value) => validateAfm(value),
+                autovalidate: _shouldValidateOnChangeVatNumber,
+                maxLength: 9,
+                onFieldSubmitted: (value) {
+                  if (!isValid(value, RegExp(r'^[0-9]+$')) || value.length != 9)
+                    setState(() => _shouldValidateOnChangeVatNumber = true);
+                  else
+                    FocusScope.of(context).requestFocus(timeToStartFocus);
+                },
                 controller: _vatNumberController,
+              ),
+              // TIMETOSTART
+              TextFormField(
+                keyboardType: TextInputType.number,
+                focusNode: timeToStartFocus,
+                controller: _timeToStartController,
+                textInputAction: TextInputAction.done,
+                decoration: InputDecoration(labelText: 'Λήξη Εργασίας'),
+                autovalidate: _shouldValidateOnChangeTimeToStart,
+                maxLength: 4,
+                validator: (value) {
+                  if (!isValid(value, RegExp(r'^[0-9]+$')) ||
+                      value.length != 4) {
+                    print('hello from validator');
+                    return 'Προσθέστε ώρα';
+                  }
+                },
+                onFieldSubmitted: (value) {
+                  if (!isValid(value, RegExp(r'^[0-9]+$')) ||
+                      value.length != 4) {
+                    setState(() => _shouldValidateOnChangeTimeToStart = true);
+                    print('hello from onFieldSubmitted');
+                  } else
+                    this.submit(context);
+                },
               ),
             ],
           ),

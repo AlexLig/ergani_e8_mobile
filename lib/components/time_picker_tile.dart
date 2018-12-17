21515718715deec82744e39f1ad9f9694fe513f0
AlexLig <ligkalex@gmail.com>
2018-12-17 22:28:34 +0200

    Update time picker button.

--- lib/components/time_picker_tile.dart
+++ lib/components/time_picker_tile.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class TimePickerTile extends StatelessWidget {
   final TimeOfDay workStart, workFinish;
-  final Function onSelectStartTime, onSelectFinishTime;
+  final dynamic onSelectStartTime, onSelectFinishTime;
   final bool isReset, outlined;
 
   TimePickerTile({
@@ -14,9 +14,7 @@ class TimePickerTile extends StatelessWidget {
     @required this.onSelectFinishTime,
     @required this.outlined,
   })  : assert(workStart != null),
-        assert(workFinish != null),
-        assert(onSelectStartTime != null),
-        assert(onSelectFinishTime != null);
+        assert(workFinish != null);
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +27,6 @@ class TimePickerTile extends StatelessWidget {
             workHour:
                 isReset ? TimeOfDay(hour: 00, minute: 00) : this.workStart,
             onPressed: isReset ? null : onSelectStartTime,
-            outlined: outlined,
           ),
           Icon(Icons.arrow_forward,
               color: isReset ? Colors.grey[400] : Colors.black),
@@ -38,7 +35,6 @@ class TimePickerTile extends StatelessWidget {
             workHour:
                 isReset ? TimeOfDay(hour: 00, minute: 00) : this.workFinish,
             onPressed: isReset ? null : onSelectFinishTime,
-            outlined: outlined,
           ),
         ],
       ),
@@ -49,73 +45,42 @@ class TimePickerTile extends StatelessWidget {
     context, {
     @required Function onPressed,
     @required TimeOfDay workHour,
-    bool outlined,
   }) {
-    return outlined
-        ? _buildUnderlinedButton(
-            context,
-            onPressed: onPressed,
-            content: _buildButtonContent(context, workHour: workHour),
-          )
-        : _buildUnderlinedButton(
-            context,
-            onPressed: onPressed,
-            content: _buildButtonContent(context, workHour: workHour),
-          );
-  }
-
-  _buildButtonContent(context, {@required TimeOfDay workHour}) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          timeToString(workHour ?? TimeOfDay(hour: 00, minute: 00)),
-          style: TextStyle(fontSize: 16.0),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 16.0),
-          // child: Icon(CupertinoIcons.time,
-          child: Icon(Icons.access_time,
-              // color: isReset ? Colors.grey[300] : Colors.grey[700]),
-              color: isReset
-                  ? Colors.grey[300]
-                  : Theme.of(context).primaryColorDark),
-        ),
-      ],
-    );
-  }
-
-  _buildRoundedButton(context, {@required onPressed, @required content}) {
-    return OutlineButton(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(50.0)),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 9.0),
-        child: content,
-      ),
-      borderSide: BorderSide(
-        color: Theme.of(context).primaryColor,
-      ),
+    return _buildUnderlinedButton(
+      context,
       onPressed: onPressed,
-      // color: Colors.grey[200],
-      highlightedBorderColor: Theme.of(context).accentColor,
+      workHour: workHour,
     );
   }
 
-  _buildUnderlinedButton(context, {@required onPressed, @required content}) {
+  _buildUnderlinedButton(context,
+      {@required onPressed, @required TimeOfDay workHour}) {
     return FlatButton(
       shape: UnderlineInputBorder(
         borderSide: BorderSide(
-            color: isReset
-                // ? Theme.of(context).primaryColorLight
-                ? Colors.grey[300]
-                : Theme.of(context).primaryColor),
+            color: isReset ? Colors.grey[300] : Theme.of(context).primaryColor),
       ),
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: content,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              timeToString(workHour ?? TimeOfDay(hour: 00, minute: 00)),
+              style: TextStyle(fontSize: 16.0),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Icon(
+                Icons.access_time,
+                color: isReset || onPressed == null
+                    ? Colors.grey[300]
+                    : Theme.of(context).primaryColorDark,
+              ),
+            ),
+          ],
+        ),
       ),
       onPressed: onPressed,
     );

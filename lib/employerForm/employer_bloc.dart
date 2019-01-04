e78b270c6cef4894e6f7baf22820e0572df5066f
AlexLig <ligkalex@gmail.com>
2019-01-04 20:52:28 +0200

    Update employer_bloc.dart

--- lib/employerForm/employer_bloc.dart
+++ lib/employerForm/employer_bloc.dart
@@ -4,16 +4,21 @@ import 'package:rxdart/rxdart.dart';
 class EmployerBloc extends Object with ValidatorBloc {
   final _nameController = BehaviorSubject<String>();
   final _afmController = BehaviorSubject<String>();
-  final _hasAmeController = BehaviorSubject<bool>();
   final _smsReceiverController = BehaviorSubject<String>();
-
+  final _ameController = BehaviorSubject<String>();
+  final _hasAmeController = BehaviorSubject<bool>();
+  final _canEditSmsReiveiverController = BehaviorSubject<bool>();
+  
   //Get data from the stream
-  Stream<bool> get hasAmeStream => _hasAmeController.stream;
+  Stream<String> get ameStream =>
+      _ameController.stream.transform(validateNumeric);
   Stream<String> get nameStream => _nameController.stream;
   Stream<String> get afmStream =>
       _afmController.stream.transform(validateNumeric);
   Stream<String> get smsReceiverStream =>
       _smsReceiverController.stream.transform(validateNumeric);
+  Stream<bool> get canEditSmsReceiverStream => _hasAmeController.stream;
+  Stream<bool> get hasAmeStream => _hasAmeController.stream;
 
   Stream<bool> get shouldSubmit => Observable.combineLatest3(
       nameStream, afmStream, smsReceiverStream, (name, afm, sms) => true);
@@ -23,11 +28,14 @@ class EmployerBloc extends Object with ValidatorBloc {
   Function(String) get updateAfm => _afmController.sink.add;
   Function(String) get updateSmsReceiver => _smsReceiverController.sink.add;
   Function(bool) get updateHasAme => _hasAmeController.sink.add;
+  Function(bool) get updateCanEdidSmsReiveiver => _hasAmeController.sink.add;
   //TODO: add ame controller and  update.
   dispose() {
     _nameController.close();
     _afmController.close();
     _hasAmeController.close();
     _smsReceiverController.close();
+    _ameController.close();
+    _canEditSmsReiveiverController.close();
   }
 }
